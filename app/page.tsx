04ea248b21909e5f6498c7daaace1b51935573b0
navi-05 <naveenthatsmyname5@gmail.com>
2023-09-20 15:16:28 +0530

    Custom Feed for logged in users and general feed

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,21 @@
-import { buttonVariants } from "@/components/ui/button";
-import { HomeIcon } from "lucide-react";
 import Link from "next/link";
+import { HomeIcon } from "lucide-react";
 
-export default function Home() {
+import { getAuthSession } from "@/lib/auth";
+
+import { buttonVariants } from "@/components/ui/button";
+import GeneralFeed from "@/components/GeneralFeed";
+import CustomFeed from "@/components/CustomFeed";
+
+export default async function Home() {
+
+  const session = await getAuthSession()
   return (
     <>
       <h1 className="font-bold text-3xl md:text-4xl">Your Feed</h1>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-y-4 md:gap-x-4 py-6">
 
-        Feed
+        { session?.user ? <CustomFeed /> : <GeneralFeed /> }
 
         {/* Subreddit info */}
         <div className="overflow-hidden h-fit rounded-lg border border-gray-200 order-first md:order-last">
---
+++ components/CustomFeed.tsx
@@ -0,0 +1,46 @@
+
+
+import { db } from "@/lib/db"
+import { getAuthSession } from "@/lib/auth"
+
+import { INFINITE_SCROLLING_PAGINATION_RESULTS } from "@/config"
+
+import PostFeed from "@/components/PostFeed"
+
+const CustomFeed = async () => {
+
+  const session = await getAuthSession()
+
+  const followedCommunites = await db.subscription.findMany({
+    where: {
+      userId: session?.user.id
+    },
+    include: {
+      subreddit: true
+    }
+  })
+
+  const posts = await db.post.findMany({
+    where: {
+      subreddit: {
+        name: {
+          in: followedCommunites.map(({ subreddit }) => subreddit.id)
+        }
+      }
+    },
+    orderBy: {
+      createdAt: 'desc'
+    },
+    include: {
+      votes: true,
+      author: true,
+      comments: true,
+      subreddit: true
+    },
+    take: INFINITE_SCROLLING_PAGINATION_RESULTS,
+  })
+
+  return <PostFeed initialPosts={posts} />
+}
+
+export default CustomFeed
