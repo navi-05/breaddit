8a9b7da8ad4c73d19911bbaf7c9f221007fbb5d7
navi-05 <naveenthatsmyname5@gmail.com>
2023-09-21 13:42:56 +0530

    Changed middleware back

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,16 @@
-export { default } from "next-auth/middleware"
+import { getToken } from 'next-auth/jwt'
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
 
-export const config = { 
-  matcher: [
-    '/r/:path*/submit', 
-    '/r/create'
-  ]
+export async function middleware(req: NextRequest) {
+  const token = await getToken({ req })
+
+  if (!token) {
+    return NextResponse.redirect(new URL('/sign-in', req.nextUrl))
+  }
+}
+
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: ['/r/:path*/submit', '/r/create'],
 }
