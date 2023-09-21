4eaa93ead3d5ec70ef4eed3b1876c0274a783cca
navi-05 <naveenthatsmyname5@gmail.com>
2023-09-21 13:23:05 +0530

    route changes for comemnting

--- app/api/subreddit/post/comment/route.ts
+++ app/api/subreddit/post/comment/route.ts
@@ -4,15 +4,6 @@ import { NextResponse } from "next/server"
 import { db } from "@/lib/db"
 import { getAuthSession } from "@/lib/auth"
 
-
-export const CommentValidator = z.object({
-  postId: z.string(),
-  text: z.string(),
-  replyToId: z.string().optional()
-})
-
-export type CommentRequest = z.infer<typeof CommentValidator>
-
 export async function PATCH(
   req: Request
 ) {
@@ -21,8 +12,7 @@ export async function PATCH(
     const session = await getAuthSession()
     if(!session?.user) return new NextResponse("Unauthorized", { status: 401 })
 
-    const body = await req.json()
-    const { postId, text, replyToId } = CommentValidator.parse(body)
+    const { postId, text, replyToId } = await req.json()
 
     if(!postId) return new NextResponse("Post Id is missing", { status: 400 })
     if(!text) return new NextResponse("Text is missing", { status: 400 })
